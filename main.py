f27cac0e5323562c150d4ce6a5cf24af0a89afbb
Sami Aden <adens7066@gmail.com>
2024-11-03 12:20:17 +0000

    Making the screen function

--- main.py
+++ main.py
@@ -2,9 +2,19 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-
+pygame.init()
+from constants import *
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen,"black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
