d7ce4b0a5769b207f38934dd076f04484a8e88b2
Sami Aden <adens7066@gmail.com>
2024-11-08 20:29:18 +0000

    Shots now actually do something, which is get rid of asteroids

--- main.py
+++ main.py
@@ -46,10 +46,14 @@ def main():
         #allows for rotation and movement, will be using groups now
         for object in updatable:
             object.update(dt)
-        for object in asteroids:
-            if object.collision(player):
+        for asteroid in asteroids:
+            if asteroid.collision(player):
                 print("Game Over!")
                 return
+            for shot in shots:
+                if asteroid.collision(shot):
+                    asteroid.kill()
+                    shot.kill()
 
 #making sure the code only runs when "python3 main.py" is run, not any modules which may have the main function imported into them
 if __name__ == "__main__":
